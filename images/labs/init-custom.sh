8b99872e97bd3e2c2d1d9d2805bd698b5dd70877
javajon <jonathan.johnson@dijure.com>
2026-08-19 19:02:04 +0000

    deploy: javajon/dijure.com@715b29fa33b7444672740bee8b001dc27ff1dadd

--- images/labs/init-custom.sh
+++ images/labs/init-custom.sh
@@ -6,11 +6,3 @@
 #   Any common background commands for all labs shall be made in init-common.sh
 #   Any custom background commands for a specific lab shall be made in init-custom.sh
 #   This file is a copied asset in index.yaml
-
-# Inject Kubernetes external ip into katacoda.yaml
-export EXTERNAL_IP=$(kubectl cluster-info | grep 'control plane' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
-envsubst < /root/katacoda-template.yaml > /root/katacoda.yaml
-rm /root/katacoda-template.yaml
-
-# Install missing packages:
-safe_install npm
